e13886c97afc2b382985d8a740126a69300c7b63
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2025-12-20 14:57:33 -0300

    estilos loader

--- src/components/shareds/loading.jsx
+++ src/components/shareds/loading.jsx
@@ -7,26 +7,10 @@ import Loader from "./Loader";
 const Loading = () => {
   const [night, setNight] = useState(false)
   const [saludo, setSaludo] = useState("")
-  /*  let saludo = ""; */
+
   const today = new Date();
   const hours = today.getHours();
-  /*   if (hours >= 7 && hours <= 12) {
-      saludo = "¡Buenos dias!"
-  
-    }
-    if (hours > 12 && hours <= 19) {
-      saludo = "¡Buenas tardes!"
-    }
-    if (hours > 19 && hours <= 24) {
-  
-      saludo = "¡Buenos noches!"
-      setnight(true)
-    }
-    if (hours >= 1 && hours < 7) {
-      saludo = "¡Buenos noches!"
-      setnight(true)
-    } */
-
+ 
   useEffect(() => {
     if ((hours >= 7 && hours <= 12) || (hours >= 1 && hours < 7)) {
       setNight(false);
